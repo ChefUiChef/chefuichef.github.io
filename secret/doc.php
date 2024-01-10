57e8453a1668b511d0495ebf5446d39c97c622f0
Chef Ui Chef <85517403+ChefUiChef@users.noreply.github.com>
2024-01-10 21:23:50 +0100

    Add files via upload

---
+++ secret/doc.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="icon" type="image/jpg" href="/stock/chefuichefx128.ico">	<!-- Tab Icon -->
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Site de Chef Ui Chef [WIP]</title>				<!-- Tab title -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> <!-- Style for social Icons -->
+		<!-- ---------- (Start) Style can be maked separately but... meh. ---------- -->
+<style>
+			@import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
+			
+			body {
+				background-color:#202124;
+				box-sizing: border-box;
+				color:#FFFFFF;
+				font-family: Arial;
+				margin: 0;
+			}
+			
+			li a {
+				color: #FFFFFF;
+				display: block;
+				padding: 8px 16px;
+				text-decoration: none;
+			}
+			
+			li a:hover:not(.active) {
+				background-color: #303134;
+				color: white;
+			}
+			
+			.container {
+				line-height: 150%;
+			}
+			
+			ul {
+				font-family: sans-serif;
+				list-style-type: none;
+				margin: 0;
+				padding: 0;
+			}
+			
+			.active {
+				background-color: #404144;
+				color: white;
+			}
+			
+			.content {
+				background-color:#FFFFFF;
+				color:#000000;
+				border-left: 1px solid #d4d4d4;
+				font-family: Times New Romans;
+				margin-left: 190px;
+				overflow: hidden;
+				padding: 1em;
+				position:fixed;
+			}
+			
+			.header {
+				align-items: center;
+				background-color: #303134;
+				display: flex;
+				justify-content: space-between;
+				padding: 15px;
+			}
+			
+			.header h1 {
+				color: #5500F0;
+				font-family: "Pacifico", cursive;
+				font-size: 30px;
+				text-shadow:
+					0 0 7px #fff,
+					0 0 10px #fff,
+					0 0 21px #fff
+			}
+			
+			.header .social a {
+				color: #222222;
+				padding: 0 5px;
+				text-decoration : none;
+			}
+			
+			.hide {
+				color:#FFFFFF;
+				text-decoration:none;
+			}
+			
+			.left {
+				float: left;
+				margin: 0;
+				padding: 1em;
+				width: 180px;
+			}
+</style>	<!-- ----------(End) Style ---------- -->
+	</head>
+	<body>
+		<div class="container">
+			<header class="header">					<!-- (Start) Top bar -->
+				<h1>Chef Ui Chef</h1>
+				<div class="social" style="padding: 1em;">	<!-- (Start) Social Links -->
+					<font <a="" class="social" size="4px"><span>Je suis sur :</span>
+						<a href="https://discord.gg/82cB9Zps7w" target="_blank">
+							<i class="fab fa-discord" style="color:#5865F2" title="Ce serveur discord (pas le mien, celui de TontonDemon)"></i>
+						</a>
+						<a href="https://www.twitch.tv/chefuichef" target="_blank">
+							<i class="fab fa-twitch" style="color:#9047FF" title="Twitch"></i>
+						</a>
+						<a href="https://twitter.com/ChefUiChef" target="_blank">
+							<i class="fab fa-twitter" style="color:#00AEEF" title="Twitter, je rt un pas des trucs de fou ici"></i>
+						</a>
+						<a href="https://www.reddit.com/user/ChefUiChef" target="_blank">
+							<i class="fab fa-reddit" style="color:#FF4500" title="Reddit TRES ! très actif dessus, J'ADORE !"></i>
+						</a>
+						<a href="https://github.com/ChefUiChef" target="_blank">
+							<i class="fab fa-github" style="color:#1B1F23" title="Github bon j'utilise pas trop"></i>
+						</a>
+						<i class="fab fa-tiktok" style="color:#404040" title="Tik... PRANK C FO ! J'irais jamais dedans MDRRR 🤣🤣🤣 (j'aime bien savoir par avance le sujet que je vais regarder) donc YT Shorts sa compte aussi"></i>
+					</font>
+				</div>						<!-- (End) Social Links -->
+			</header>						<!-- (End) Top bar -->
+			<aside class="left">					<!-- (Start) Left bar -->
+				<img src="/stock/chefuichef.jpg" width="160px">
+				<ul>						<!-- (Start) Interactive Buttons -->
+					<li>
+						<a href="..\">Accueil</a>
+					</li>
+					<li>
+						<a href="/Contact">Contact</a>
+					</li>
+					<li>
+						<a href="https://scoresaber.com/u/76561198397022680" target="_blank">ScoreSaber</a>
+					</li>
+					<li>
+						<a href="https://www.beatleader.xyz/u/76561198397022680" target="_blank">BeatLeader</a>
+					</li>
+					<li>
+						<a href="7,jls)_(éQwO§²¤§">DIY Tracker Project</a>
+					</li>
+				</ul>						<!-- (End) Interactive Buttons -->
+				<br>
+				<br>
+				<p>...</p>
+			</aside>						<!-- (End) Left bar -->
+			<main class="content">
+<?php
+$a[] = "a";
+$a[] = "c";
+$a[] = "b";
+
+echo $a[1];
+?>
+			</main>
+		</div>
+	</body>
+</html>
