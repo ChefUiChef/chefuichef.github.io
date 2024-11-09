5c56cfae14489c370a1d7174b90a308f22cb5b68
Chef Ui Chef <85517403+ChefUiChef@users.noreply.github.com>
2024-11-09 15:42:00 +0100

    Add files via upload

---
+++ textList.js
@@ -0,0 +1,15 @@
+// List
+const textList = [
+	"Mise à jour de votre écran... NE QUITTEZ PAS!",
+	"Téléchargement de données depuis l'Inter WEB !",
+	"Le café est chaud.",
+	"helo.jpg",
+	/*
+	"Tip : To power off your computer, you can click on the \"Windows\" logo, click on the power logo and shut down ! 😉",
+	"Tip : To copy and paste something, you can Right-click on something than copy and after, right click and click \"Paste\" 😉",
+	"Tip : To launch something, tou can double-click on the icon 😉",
+	"Tip : idk i forgot...",
+	/**/
+	"Il est temps de faire une pause.",
+	"\"je suis gay\" - Lukas"
+];
